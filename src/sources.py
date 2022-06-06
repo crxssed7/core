9c10a8a6b08fa0596cc0ad8d392bed2ebcbb1736
Tanveer Najib <tanveernajib@gmail.com>
2022-06-06 17:14:51 +0100

    update trailing slash on sources.py endpoints

--- src/sources.py
+++ src/sources.py
@@ -2,16 +2,16 @@ SOURCES = {
     'comics': [
         {
             'name': 'Comicastle',
-            'endpoint': '/comics/comicastle',
-            'search_endpoint': '/comics/comicastle/s/<query>',
+            'endpoint': '/comics/comicastle/',
+            'search_endpoint': '/comics/comicastle/s/<query>/',
             'type': 'stream',
             'logo': 'https://comicastle.org/upload/logo/cc__logo.png',
             'url': 'https://comicastle.org/home'
         },
         {
             'name': 'ComicExtra',
-            'endpoint': '/comics/comicextra',
-            'search_endpoint': '/comics/comicextra/s/<query>',
+            'endpoint': '/comics/comicextra/',
+            'search_endpoint': '/comics/comicextra/s/<query>/',
             'type': 'stream',
             'logo': 'https://www.comicextra.com/images/site/front/logo.png',
             'url': 'https://www.comicextra.com'
@@ -20,16 +20,16 @@ SOURCES = {
     'manga': [
         {
             'name': 'GogoManga',
-            'endpoint': '/manga/gogomanga',
-            'search_endpoint': '/manga/gogomanga/s/<query>',
+            'endpoint': '/manga/gogomanga/',
+            'search_endpoint': '/manga/gogomanga/s/<query>/',
             'type': 'stream',
             'logo': 'https://gogomanga.fun/wp-content/uploads/2021/06/LogoMakr.png',
             'url': 'https://gogomanga.fun'
         },
         {
             'name': 'MangaPill',
-            'endpoint': '/manga/mangapill',
-            'search_endpoint': '/manga/mangapill/s/<query>',
+            'endpoint': '/manga/mangapill/',
+            'search_endpoint': '/manga/mangapill/s/<query>/',
             'type': 'stream',
             'logo': 'https://res.cloudinary.com/crxssed/image/upload/v1654273328/mpill_mumedn.png',
             'url': 'https://mangapill.com'
